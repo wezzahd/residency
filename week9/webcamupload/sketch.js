80e7c4cfcf735b1fe63623b2ba1f76d528f88b13
Wesley Dowling <wezzhad@gmail.com>
2020-04-16 09:53:43 +1000

    webcam flip

--- week9/webcamupload/sketch.js
+++ week9/webcamupload/sketch.js
@@ -1,78 +1,79 @@
 var img;
 var drawpg;
 var colour;
+var text;
 
 
+function firebasesetup() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyAbb8-7skMg99nzAlvUaqR6vfQvD7q_7Vs",
+    authDomain: "test-a201f.firebaseapp.com",
+    databaseURL: "https://test-a201f.firebaseio.com",
+    projectId: "test-a201f",
+    storageBucket: "test-a201f.appspot.com",
+    messagingSenderId: "120555329679",
+    appId: "1:120555329679:web:a8734f69a5da95480df8fa",
+    measurementId: "G-QLSEEEE02P"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
-function firebasesetup(){
-	var firebaseConfig = {
-		apiKey: "AIzaSyAbb8-7skMg99nzAlvUaqR6vfQvD7q_7Vs",
-		authDomain: "test-a201f.firebaseapp.com",
-		databaseURL: "https://test-a201f.firebaseio.com",
-		projectId: "test-a201f",
-		storageBucket: "test-a201f.appspot.com",
-		messagingSenderId: "120555329679",
-		appId: "1:120555329679:web:a8734f69a5da95480df8fa",
-		measurementId: "G-QLSEEEE02P"
-	};
-	// Initialize Firebase
-	firebase.initializeApp(firebaseConfig);
-	firebase.analytics();
+  database = firebase.database();
 
-	database = firebase.database();
-
-	drawpg = createGraphics(width,height);
+  drawpg = createGraphics(width, height);
 
 }
 
-function drawellipse(){
-	noStroke();
-colour = img.get(mouseX,mouseY);
+function drawellipse() {
+  noStroke();
+  colour = img.get(mouseX, mouseY);
 
-		fill(255,127);
-		ellipse(mouseX,mouseY,120,120);
-		fill(colour);
-		ellipse(mouseX,mouseY,100,100);
-  // p.blendMode(ADD);
-	// p.image(img,0,0);
-
-
-
+  fill(255, 127);
+  ellipse(mouseX, mouseY, 120, 120);
+  fill(colour);
+  ellipse(mouseX, mouseY, 100, 100);
 }
 
 
 function setup() {
   createCanvas(640, 480);
   img = createCapture(VIDEO);
-  img.size(width,height);
+  img.size(width, height);
   img.hide();
-	firebasesetup();
+  firebasesetup();
+  didactic();
+}
 
+function didactic() {
+  text = createDiv('Donate-A-Pixel.  Move mouse to select pixel.  Mouse click to send pixel info.')
+  text.class("didactic");
+  text.position(0, 0);
 }
 
 function draw() {
 
+  push();
+  translate(width, 0);
+  scale(-1, 1);
+  image(img, 0, 0); //background
+  pop();
 
-image(img,0,0);//background
-drawellipse();
+  drawellipse();
 
-//blendMode(REPLACE);
-//image(drawpg,0,0); // foreground
 
- }
+}
 
- function mousePressed() {
+function mousePressed() {
 
-	 var data = {
-	   mouseX_loc: mouseX,
-	   mouseY_loc: mouseY,
-		 colour_loc: colour
-	 }
+  var data = {
+    mouseX_loc: mouseX,
+    mouseY_loc: mouseY,
+    colour_loc: colour
+  }
 
- //saveFrames('out', 'png', 1, 1, data => {
-	 var test = database.ref('test');
+  var test = database.ref('test');
 
-	 test.push(data);
-	 console.log(data);
-// });
- }
+  test.push(data);
+  console.log(data);
+}
